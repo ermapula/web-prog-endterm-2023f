19286184843379a347ca937e1555b78f619230b3
Ermek <78418830+ermapula@users.noreply.github.com>
2023-12-06 13:27:30 +0600

    upd: DML.sql hashed pwds

--- database/DML.sql
+++ database/DML.sql
@@ -1,10 +1,10 @@
 INSERT INTO users (user_id, user_firstname, user_lastname, user_nickname, user_password, user_email, user_gender, user_birthdate, user_status, user_about, user_hometown) VALUES
 (1, 'Test', 'Test', '', '098f6bcd4621d373cade4e832627b4f6', 'test@test.test', 'M', '1996-01-01', '', '', ''),
-(2, 'Ermek', 'Tolymbekov', NULL, 'password', 'ermek@gmail.com', 'M', '2003-07-03', NULL, NULL, NULL),
-(3, 'Bayashat', 'Tokmukhamet', NULL, 'password', 'bayashat@gmail.com', 'M', '2000-01-01', 'S', NULL, NULL),
-(4, 'John', 'Doe', NULL, 'password', 'john@gmail.com', 'M', '1996-01-18', NULL, NULL, NULL),
-(5, 'Әсем', 'Нұрлыбайова', NULL, 'password', 'asem@gmail.com', 'F', '1994-04-18', 'M', NULL, NULL),
-(6, 'Сәбит', 'Қырықбай', NULL, 'password', 'sabit@gmail.com', 'M', '1994-06-06', NULL, NULL, NULL),
+(2, 'Ermek', 'Tolymbekov', NULL, '5f4dcc3b5aa765d61d8327deb882cf99', 'ermek@gmail.com', 'M', '2003-07-03', NULL, NULL, NULL),
+(3, 'Bayashat', 'Tokmukhamet', NULL, '5f4dcc3b5aa765d61d8327deb882cf99', 'bayashat@gmail.com', 'M', '2000-01-01', 'S', NULL, NULL),
+(4, 'John', 'Doe', NULL, '5f4dcc3b5aa765d61d8327deb882cf99', 'john@gmail.com', 'M', '1996-01-18', NULL, NULL, NULL),
+(5, 'Әсем', 'Нұрлыбайова', NULL, '5f4dcc3b5aa765d61d8327deb882cf99', 'asem@gmail.com', 'F', '1994-04-18', 'M', NULL, NULL),
+(6, 'Сәбит', 'Қырықбай', NULL, '5f4dcc3b5aa765d61d8327deb882cf99', 'sabit@gmail.com', 'M', '1994-06-06', NULL, NULL, NULL),
 (7, 'Atym', 'Jonim', '', '5f4dcc3b5aa765d61d8327deb882cf99', 'mail@mail.com', 'M', '1996-01-01', '', '', '');
 
 INSERT INTO friendship (user1_id, user2_id, friendship_status) VALUES
